2193eb0a9144ea904fa6d778c7544a540d621b7b
HN026 <huzaifanaseer026@gmail.com>
2024-03-22 23:15:00 +0530

    Tcp server Initialised

---
+++ http_tcpServer.h
@@ -0,0 +1,32 @@
+#ifndef INCLUDED_HTTP_TCPSERVER
+#define INCLUDED_HTTP_TCPSERVER
+
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <sys/socket.h>
+
+namespace http {
+class TcpServer {
+  public:
+    TcpServer(std::string ip_address, int port);
+    ~TcpServer();
+
+  private:
+    std::string m_ip_address;
+    int m_port;
+    int m_socket;
+    int m_new_socket;
+    long m_incomingMessage;
+    struct sockaddr_in m_socketAddress;
+    unsigned int m_socketAddress_len;
+    std::string m_serverMessage;
+
+    int startServer();
+    void closeServer();
+    std::string buildResponse();
+};
+} // namespace http
+
+#endif
